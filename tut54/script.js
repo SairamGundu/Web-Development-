1625a2b573b9b6eb45a8e3fbfd2db0fae26aae94
SairamGundu <165833309+SairamGundu@users.noreply.github.com>
2024-04-19 23:31:09 +0530

    Add files via upload

---
+++ tut54/script.js
@@ -0,0 +1,17 @@
+alert("Hello world");
+
+console.log("Code is running");
+console.log("Code is also running");
+console.log("Code is baghing running");
+
+var a = prompt("ENter your number");
+console.log("Your number is " + a);
+
+var b = confirm("Do you want to exit?");
+if (b) {
+    alert("Exiting");
+} else {
+    console.log("Remain in the page");
+}
+
+document.title("Help");
---
+++ tut55/script.js
@@ -0,0 +1,43 @@
+console.log("This is tut 55");
+let a = 5;
+a = a + 1;
+var b = 6;
+var c = "Sairam";
+var _a = "Navin";
+//var 55a = "TRohan"; //invalid
+
+// console.log(a + b + 8);
+
+// console.log(typeof a, typeof b, typeof c);
+{
+    // var a1 = 66;
+    console.log(a);
+}
+console.log(a);
+
+// const a1 = 6;
+// a1 = a1 + a; // Because a1 is constant
+
+let x = "Sairam Gundu";
+let y = 22;
+let z = 4.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+
+let o = {
+    "name": "Sairam",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
