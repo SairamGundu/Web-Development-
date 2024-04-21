f867e4b0bd9e3baaee02b54f3e8d3a8ebe59dc46
SairamGundu <165833309+SairamGundu@users.noreply.github.com>
2024-04-21 23:28:46 +0530

    Add files via upload

---
+++ tut60/index.js
@@ -0,0 +1,38 @@
+console.log("This is String")
+
+let a = "Sairam"
+
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+console.log(a.length)
+
+let name = "Sairam"
+let sname = "gundu"
+
+console.log("His name is " + name + " and his surname is " + sname);
+
+console.log(`his name is ${name} and his surname is ${sname}`)
+
+let b = "Saikiran"
+let c = "namani"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 5))
+console.log(b.slice(1))
+console.log(b.replace("kiran", "Potodu"))
+console.log(b.concat(c))
+console.log(b.concat(c, "Ramu", "Badwa"))
+
+
+
+
+
+
+
